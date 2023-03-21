926a183bffe6a503432aeab72fa5fa460d18f3ac
mymotherati <100pofarengeitu@gmail.com>
2023-03-21 18:18:40 +0300

    deleted cache

--- contact/urls.py
+++ contact/urls.py
@@ -4,6 +4,6 @@ from django.views.decorators.cache import cache_page
 
 
 urlpatterns = [
-	path('contact/', cache_page(60 * 15)(views.ContactView.as_view()), name='contact'),
+	path('contact/', views.ContactView.as_view(), name='contact'),
 	path('feedback/', views.CreateComment.as_view(), name='feedback')
 ]
